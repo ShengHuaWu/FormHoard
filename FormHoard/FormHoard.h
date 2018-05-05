ba76ba52949d843af12617719426ac4999f053b8
ShengHua Wu <fantasy0404@gmail.com>
2018-05-05 21:22:18 +0200

    Initialize project.

---
+++ FormHoard/FormHoard.h
@@ -0,0 +1,19 @@
+//
+//  FormHoard.h
+//  FormHoard
+//
+//  Created by ShengHua Wu on 05.05.18.
+//  Copyright © 2018 ShengHua Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FormHoard.
+FOUNDATION_EXPORT double FormHoardVersionNumber;
+
+//! Project version string for FormHoard.
+FOUNDATION_EXPORT const unsigned char FormHoardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FormHoard/PublicHeader.h>
+
+
