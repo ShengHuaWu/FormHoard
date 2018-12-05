231bdbffc7f9c970ef77475d36d8c8fc8892e835
ShengHua Wu <swu1@wayfair.com>
2018-12-05 18:06:16 +0100

    Create Text cell and Texting.

--- Example/ExampleViewController.swift
+++ Example/ExampleViewController.swift
@@ -18,6 +18,16 @@ struct Toggling {
     }
 }
 
+struct Texting {
+    let title: String
+    var text: String
+    
+    init(title: String, text: String) {
+        self.title = title
+        self.text = text
+    }
+}
+
 func buildToggleCell(with driver: Driver<Toggling>, from collectionView: UICollectionView, for indexPath: IndexPath) -> ToggleCell {
     guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ToggleCell.cellIdentifier, for: indexPath) as? ToggleCell else {
         fatalError("Unrecognized cell type")
@@ -27,6 +37,20 @@ func buildToggleCell(with driver: Driver<Toggling>, from collectionView: UIColle
     cell.switcher.isOn = driver.getValue(from: \.enabled)
     cell.valueChange = { isOn in
         driver.update(\.enabled, with: isOn)
+    }
+    
+    return cell
+}
+
+func buildTextCell(with driver: Driver<Texting>, from collectionView: UICollectionView, for indexPath: IndexPath) -> TextCell {
+    guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TextCell.cellIdentifier, for: indexPath) as? TextCell else {
+        fatalError("Unrecognized cell type")
+    }
+    
+    cell.titleLabel.text = driver.getValue(from: \.title)
+    cell.textField.text = driver.getValue(from: \.text)
+    cell.valueChange = { text in
+        driver.update(\.text, with: text)
     }
     
     return cell
@@ -58,7 +82,7 @@ final class ToggleCell :UICollectionViewCell {
     private(set) lazy var titleLabel: UILabel = {
         let label = UILabel(frame: .zero)
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = UIFont.systemFont(ofSize: 24, weight: .medium)
+        label.font = .systemFont(ofSize: 20, weight: .medium)
         
         return label
     }()
@@ -97,9 +121,60 @@ final class ToggleCell :UICollectionViewCell {
     }
 }
 
+final class TextCell: UICollectionViewCell {
+    static let cellIdentifier = "TextCell"
+    
+    private(set) lazy var titleLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 20, weight: .medium)
+        
+        return label
+    }()
+    
+    private(set) lazy var textField: UITextField = {
+        let textField = UITextField(frame: .zero)
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.font = .systemFont(ofSize: 24, weight: .medium)
+        textField.addTarget(self, action: #selector(textDidChange), for: .editingChanged)
+        
+        return textField
+    }()
+    
+    var valueChange: ((String) -> ())?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(textField)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            titleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: 8),
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            textField.topAnchor.constraint(equalTo: titleLabel.topAnchor),
+            textField.bottomAnchor.constraint(equalTo: titleLabel.bottomAnchor),
+            textField.leadingAnchor.constraint(equalTo: contentView.centerXAnchor, constant: 8),
+            textField.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16)
+            ])
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc func textDidChange(_ sender: UITextField) {
+        guard let text = sender.text else { return }
+        
+        valueChange?(text)
+    }
+}
+
 final class ExampleViewController: UICollectionViewController {
     // MARK: Properties
-    private let driver = Driver<Toggling>(element: Toggling(title: "Toggle"))
+    private let togglingDriver = Driver<Toggling>(element: Toggling(title: "Toggle"))
+    private let textingDriver = Driver<Texting>(element: Texting(title: "Text", text: "Nothing"))
 
     // MARK: View Life Cycle
     override func viewDidLoad() {
@@ -108,7 +183,8 @@ final class ExampleViewController: UICollectionViewController {
         let flowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout
         flowLayout?.itemSize = CGSize(width: collectionView.frame.width, height: 44)
         
-        collectionView!.register(ToggleCell.self, forCellWithReuseIdentifier: ToggleCell.cellIdentifier)
+        collectionView.register(ToggleCell.self, forCellWithReuseIdentifier: ToggleCell.cellIdentifier)
+        collectionView.register(TextCell.self, forCellWithReuseIdentifier: TextCell.cellIdentifier)
     }
 }
 
@@ -119,11 +195,17 @@ extension ExampleViewController {
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return 2
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = buildToggleCell(with: driver, from: collectionView, for: indexPath)
-        return cell
+        switch indexPath.item {
+        case 0:
+            return buildToggleCell(with: togglingDriver, from: collectionView, for: indexPath)
+        case 1:
+            return buildTextCell(with: textingDriver, from: collectionView, for: indexPath)
+        default:
+            fatalError("Too many cells")
+        }
     }
 }
